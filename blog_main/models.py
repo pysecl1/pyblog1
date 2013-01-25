3ea6217f2f3435acf2eb1738953e7d337c29f50d
Victor Mireyev <victor.mireyev@gmail.com>
2013-01-25 20:13:50 +0200

    init blog

---
+++ blog_main/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+gender_choices = (('m', 'male'), ('f', 'female'))
+
+class Blogs (models.Model):
+    pass
+    user_id = models.ForeignKey()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    logo = models.FileField(upload_to='logo', blank=True)
+    created_at = models.DateField(default='today')
+
+
+
+class Users (models.Model):
+    pass
+    username = models.CharField(max_length=50)
+    gender = models.CharField(max_length=15, choices=gender_choices)
+    b_day = models.DateField(blank=True, null=True)
+
+
+class Posts (models.Model):
+    pass
+    blog_id = models.ForeignKey()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user_id =
