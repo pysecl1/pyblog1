0566d0ba20ff845f2857ab934b80e2c29b6e401e
Pasha <osmak93@mail.ru>
2013-01-28 21:14:20 +0200

    models_settings

--- blog_main/fixture_gen.py
+++ blog_main/fixture_gen.py
@@ -1,1 +1,12 @@
 __author__ = 'User'
+from fixture_generator import fixture_generator
+
+from models import Blogs, Posts
+
+
+@fixture_generator(Blogs)
+def test_blogs():
+    """
+     create test blogs collection
+    """
+    blog1 = Blogs.objects.create(title="Test blog 1", description="my test blog")
