70e6fe29a66e59bf409729a03748a58e9600d8e7
Victor Mireyev <victor.mireyev@gmail.com>
2013-01-30 19:07:00 +0200

    kom settings

---
+++ blog/database_settings.py
@@ -0,0 +1,11 @@
+__author__ = 'USer'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'D:/python/new_p-master/new_p.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
