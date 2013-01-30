fe30d312acdd7226fb2777735f235a8203c5009a
hsn <hsn@crimea.com>
2013-01-30 20:56:27 +0200

    uncomment urls

--- blog/urls.py
+++ blog/urls.py
@@ -10,14 +10,14 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.foo.urls')),
     ##-
     url(r'^$','sprofile.views.main'),
-    #url(r'^login/$','sprofile.views.login'),
-    #url(r'^logout/$','sprofile.views.logout'),
+    url(r'^login/$','sprofile.views.login'),
+    url(r'^logout/$','sprofile.views.logout'),
     url(r'^registration/$','sprofile.views.registration'),
-    #url(r'^profile/$','sprofile.views.profile'),
+    url(r'^profile/$','sprofile.views.profile'),
 
-    #url(r'^users_list/$','sprofile.views.users_list'),
+    url(r'^users_list/$','sprofile.views.users_list'),
 
-    #url(r'^user_profile/(?P<user_id>\d+)$','sprofile.views.user_profile'),
+    url(r'^user_profile/(?P<user_id>\d+)$','sprofile.views.user_profile'),
     ##-
 
     # Uncomment the admin/doc line below to enable admin documentation:
