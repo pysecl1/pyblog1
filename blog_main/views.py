81b1ae815cbd4d273cc75d5794cd0a2950144243
Pasha <osmak93@mail.ru>
2013-02-01 19:49:38 +0200

    my profile

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,14 @@
 # Create your views here.
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render , redirect
+from django.contrib.auth.models import User
 
 def home(request):
     return render_to_response("blog/index.html", context_instance=RequestContext(request))
+
+def profile(request, id=None):
+    if id is not None:
+        user=User.objects.get(pk=id)
+    else:
+        user=request.user
+    return render(request,'registration/profile.html',{'profile':user})
