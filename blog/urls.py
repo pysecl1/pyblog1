392ab5685a1e2fb8cbcd868f8a9e5c021e76b961
Pasha <osmak93@mail.ru>
2013-02-01 19:09:20 +0200

    redaction profile

--- blog/urls.py
+++ blog/urls.py
@@ -11,13 +11,13 @@ urlpatterns = patterns('',
     ##-
     url(r'^$','sprofile.views.main'),
     url(r'^login/$','sprofile.views.login'),
-    url(r'^logout/$','sprofile.views.logout'),
+    url(r'^logout/$','sprofile.views.logout',name='logout'),
     url(r'^registration/$','sprofile.views.registration'),
     url(r'^profile/$','sprofile.views.profile'),
 
     #url(r'^users_list/$','sprofile.views.users_list'),
 
-    #url(r'^user_profile/(?P<user_id>\d+)$','sprofile.views.user_profile'),
+    url(r'^user_profile/(?P<user_id>\d+)$','sprofile.views.user_profile'),
     ##-
 
     # Uncomment the admin/doc line below to enable admin documentation:
