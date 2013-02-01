93096965c40776341de022bc29493a5374ce9a43
Pasha <osmak93@mail.ru>
2013-02-01 20:37:42 +0200

    my profile start korrect

--- blog_main/views.py
+++ blog_main/views.py
@@ -6,9 +6,3 @@ from django.contrib.auth.models import User
 def home(request):
     return render_to_response("blog/index.html", context_instance=RequestContext(request))
 
-def profile(request, id=None):
-    if id is not None:
-        user=User.objects.get(pk=id)
-    else:
-        user=request.user
-    return render(request,'registration/profile.html',{'profile':user})
