052380b6901fc96ab207578a823bb7ff944a95a2
Victor Mireyev <victor.mireyev@gmail.com>
2013-02-04 18:37:22 +0200

    office fixes

--- blog_main/admin.py
+++ blog_main/admin.py
@@ -1,9 +1,9 @@
-from blog_main.models import Users, Blogs, Posts
+#from blog_main.models import Users, Blogs, Posts
 
 __author__ = 'USer'
 
 from django.contrib import admin
 
-admin.site.register(Users)
-admin.site.register(Blogs)
-admin.site.register(Posts)
+#admin.site.register(Users)
+#admin.site.register(Blogs)
+#admin.site.register(Posts)
