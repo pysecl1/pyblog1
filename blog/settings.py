dcecde4e658137b7d8ca8ff21abdf489f7b95d5a
Alexander <pysecl1@yandex.ua>
2013-03-01 20:29:13 +0200

    fix static path

--- blog/settings.py
+++ blog/settings.py
@@ -65,20 +65,20 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), "../static/").replace('\\','/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = os.path.join(os.path.dirname(__file__), "../static/").replace('\\','/')#'static/'
-
+#STATIC_URL = os.path.join(os.path.dirname(__file__), "../static/").replace('\\','/')#'static/'
+STATIC_URL ='/static/';
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 
-    #APP_DIR+'static',
-    'c:/work_temp/web/phyton/pyblog1/static',
+    APP_DIR+'static',
+    #'c:/work_temp/web/phyton/pyblog1/static',
     )
 
 
